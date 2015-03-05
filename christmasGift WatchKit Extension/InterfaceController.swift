ab074cfc10ba6f3deccd0c400f93797859764c08
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-03-05 11:54:43 -0800

    first commit

---
+++ christmasGift WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  christmasGift WatchKit Extension
+//
+//  Created by Vineet Tiwari on 2015-03-05.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
