c60c1c555a5b8bc01e526b39532783549c142970
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-03-05 10:55:21 -0800

    Initial Commit

---
+++ christmasGift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  christmasGift
+//
+//  Created by Vineet Tiwari on 2015-03-05.
+//  Copyright (c) 2015 vinny.co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
